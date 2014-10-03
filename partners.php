9ffa775b7cacd95fe84be0afa4adfef66e83029a
Scott Richards <scott.srichards@gmail.com>
2014-10-03 12:52:55 -0700

    Icons, Partners Page, Rules

---
+++ partners.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>Speakers &amp; Partners</title>
+    <link href="_css/live2014.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Speakers &amp; Partners</h1>
+<p><img src="_img/partners/Forrester_Research_Logo.png" width="158" height="59" alt="Forrester"></p>
+<p><span class="partner">Diego Lo Giudice</span><br>
+  <span class="title">Gartner Principal Analyst, APPLICATION DEVELOPMENT &amp; DELIVERY</span><br>
+  Diego primarily contributes to and advises on Forrester's offerings for Application Development &amp; Delivery Professionals. He partners with Forrester's global application leaders and is a leading expert on SDLC processes and practices, covering topics such as Agile development, Agile and Lean transformations, Agile development sourcing strategies and services, Agile testing practices and tools, DevOps, and software testing and quality, with a key focus on systems of engagement. Diego also covers software delivery metrics, artificial intelligence, and open source governance.</p>
+<p><a href="http://www.plm.automation.siemens.com/en_us/"><img src="_img/partners/siemens.png" width="151" height="32" alt="Siemens"></a></p>
+<p><span class="partner">Pascal Vera</span><br>
+  <span class="title">Siemens Teamcenter, Product Management</span><br>
+Pascal is the Teamcenter Product Manager for Mechatronics/High-Tech Electronics. Holding a doctoral degree in industrial software engineering, he has experience in electronics and embedded systems.</p>
+<p><a href=" http://www.agcocorp.com/"><img src="_img/partners/agco.jpg" width="151" height="58" alt="agco"></a></p>
+<p>As a pure-play agriculture company, AGCO's research and development initiatives are entirely focused on agricultural innovation. In short, AGCO is dedicated to helping our customers achieve more with fewer resources. From productivity-boosting hybrid combines to telemetry-based tracking, AGCO supplies agribusiness and independent farmers with the tools and technology needed to efficiently meet growing demand for food, fuel and fiber worldwide.</p>
+<p>&nbsp;</p>
+</body>
+</html>
