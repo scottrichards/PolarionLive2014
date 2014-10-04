e59deb037a2fe227e26939e9bc9c2b7d377d7372
Scott Richards <scott.srichards@gmail.com>
2014-10-03 17:19:58 -0700

    copy changes, make map small

--- partners.php
+++ partners.php
@@ -5,11 +5,16 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<title>Speakers &amp; Partners</title>
     <link href="_css/live2014.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+    p {
+	font-size: small;
+}
+    </style>
 </head>
 <body>
 <h1><img src="_img/partners/Forrester_Research_Logo.png" width="158" height="59" alt="Forrester"></h1>
 <p><span class="partner">Diego Lo Giudice</span><br>
-  <span class="title">Gartner Principal Analyst, APPLICATION DEVELOPMENT &amp; DELIVERY</span><br>
+  <span class="title">Forrester Principal Analyst, Application Development &amp; Delivery</span><br>
   Diego primarily contributes to and advises on Forrester's offerings for Application Development &amp; Delivery Professionals. He partners with Forrester's global application leaders and is a leading expert on SDLC processes and practices, covering topics such as Agile development, Agile and Lean transformations, Agile development sourcing strategies and services, Agile testing practices and tools, DevOps, and software testing and quality, with a key focus on systems of engagement. Diego also covers software delivery metrics, artificial intelligence, and open source governance.</p>
 <p><a href="http://www.plm.automation.siemens.com/en_us/"><img src="_img/partners/siemens.png" width="151" height="32" alt="Siemens"></a></p>
 <p><span class="partner">Pascal Vera</span><br>
