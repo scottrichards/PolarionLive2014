a9e8ba9e07daa38fdc7eff8eec09d207e0c1874a
Scott Richards <scott.srichards@gmail.com>
2014-10-05 14:34:55 -0700

    Changes from Vera

--- partners.php
+++ partners.php
@@ -12,16 +12,71 @@
     </style>
 </head>
 <body>
-<h1><img src="_img/partners/Forrester_Research_Logo.png" width="158" height="59" alt="Forrester"></h1>
+<h1><a href="https://www.forrester.com/home/"><img src="_img/partners/Forrester_Research_Logo.png" width="150" height="56" alt="Forrester"></a></h1>
 <p><span class="partner">Diego Lo Giudice</span><br>
-  <span class="title">Forrester Principal Analyst, Application Development &amp; Delivery</span><br>
-  Diego primarily contributes to and advises on Forrester's offerings for Application Development &amp; Delivery Professionals. He partners with Forrester's global application leaders and is a leading expert on SDLC processes and practices, covering topics such as Agile development, Agile and Lean transformations, Agile development sourcing strategies and services, Agile testing practices and tools, DevOps, and software testing and quality, with a key focus on systems of engagement. Diego also covers software delivery metrics, artificial intelligence, and open source governance.</p>
-<p><a href="http://www.plm.automation.siemens.com/en_us/"><img src="_img/partners/siemens.png" width="151" height="32" alt="Siemens"></a></p>
-<p><span class="partner">Pascal Vera</span><br>
+  <span class="title">Principal Analyst, Application Development &amp; Delivery</span><br>
+  Diego advises on Forrester's offerings for Application Development &amp; Delivery Professionals partners with Forrester's global application leaders, and is a leading expert on SDLC processes and practices, covering topics such as Agile development, Agile and Lean transformations, Agile development sourcing strategies and services, Agile testing practices and tools, DevOps, and software testing and quality, with a key focus on systems of engagement.<br>
+  <br>
+</p>
+<p><a href="http://www.plm.automation.siemens.com/en_us/"><img src="_img/partners/siemens.png" width="150" height="32" alt="Siemens"><br>
+</a><span class="partner"><br>
+Pascal Vera</span><br>
   <span class="title">Siemens Teamcenter, Product Management</span><br>
-Pascal is the Teamcenter Product Manager for Mechatronics/High-Tech Electronics. Holding a doctoral degree in industrial software engineering, he has experience in electronics and embedded systems.</p>
-<p><a href=" http://www.agcocorp.com/"><img src="_img/partners/agco.jpg" width="151" height="58" alt="agco"></a></p>
-<p>As a pure-play agriculture company, AGCO's research and development initiatives are entirely focused on agricultural innovation. In short, AGCO is dedicated to helping our customers achieve more with fewer resources. From productivity-boosting hybrid combines to telemetry-based tracking, AGCO supplies agribusiness and independent farmers with the tools and technology needed to efficiently meet growing demand for food, fuel and fiber worldwide.</p>
-<p>&nbsp;</p>
+Pascal is the Teamcenter Product Manager for Mechatronics/High-Tech Electronics. Holding a doctoral degree in industrial software engineering, he has extensive experience in electronics and embedded systems.<br>
+<br>
+</p>
+<p><a href=" http://www.agcocorp.com/"><img src="_img/partners/agco.jpg" width="150" height="57" alt="agco"></a></p>
+<p>As a pure-play agriculture company, AGCO's research and development initiatives are entirely focused on agricultural innovation. In short, AGCO is dedicated to helping our customers achieve more with fewer resources. From productivity-boosting hybrid combines to telemetry-based tracking, AGCO supplies agribusiness and independent farmers with the tools and technology needed to efficiently meet growing demand for food, fuel and fiber worldwide.<br>
+  <br>
+</p>
+<p><a href="http://www.lantiq.com/"><img src="_img/partners/lantiq.jpg" width="151" height="42" alt="Lantiq"></a></p>
+<p>Lantiq develops and provides solutions that significantly reduce the energy consumption of broadband equipment. For example, our EASY reference designs for Broadband CPE solutions  lead the market in fulfilling the European Code of Conduct (CoC) for energy efficiency and support the IEEE Energy Efficient Ethernet (EEE) standard.<br>
+  <br>
+</p>
+<p><a href="http://www.wavelight.de/?cjt=VCzDQz6SHVQAAE2wcM8AAAAA"><img src="_img/partners/wavelight.jpg" width="150" height="42" alt="WaveLight"></a></p>
+<p>Die WaveLight GmbH ist ein Tochterunternehmen von Alcon, dem Unternehmensbereich für Augenheilkunde innerhalb der Novartis-Gruppe. Das Unternehmen ist weltweit führend in der Entwicklung und Produktion moderner Diagnose- und Operationstechnologien zur Korrektur von Fehlsichtigkeiten mit Augenlasern. <br>
+  <br>
+</p>
+<p><a href="https://www.phoenixcontact.com/online/portal/pc"><img src="_img/partners/phoenixcontact.jpg" width="150" height="46" alt="Phoenix Contact"></a></p>
+<p>PHOENIX CONTACT Electronics GmbH is a global market leader and innovator in the field of electrical engineering and automation. We are also a family company working responsibly to shape the future. Our teams manage 250 projects through Polarion, which is used by 400 users, part of which are external service providers. Teams can easily collaborate thanks to the single web-based platform in which data is stored and documents are unified, avoiding misunderstandings and confusion.<br>
+  <br>
+</p>
+<p><a href="http://www.kuester.net/en/"><img src="_img/partners/kster_logo.gif" width="150" height="56" alt="K&uuml;ster"></a></p>
+<p>Küster is a global family business with a tradition as a supplier to the automotive industry. Our entrepreneurial activity follows the principle of sustainability. For us, this means: Long-term thinking instead of short-term pursuit of profit. As a full service supplier, we produce intelligent electro-mechanical actuation and adjustment systems, in-house developed electronics and actuating elements. <br>
+  <br>
+</p>
+<p><a href="http://www.sparxsystems.de/"><img src="_img/partners/sparx.jpg" width="150" height="32"></a><br>
+  <br>
+Service-Provider of Sparx Enterprise Architect + Polarion ALM for efficient, cost-effective development processes,  Sparsystems offers their customers a perfect assistance for the well known UML Modeling Tool  “Enterprise Architect” and seamless integration with Polarion solutions.  This will help you getting your EA licence as well as we offer you whitepapers, help through the installing process and we are offering UML Trainings and customized Workshops.  <br>
+<br>
+</p>
+<p><a href="http://www.vectorcast.com/"><img src="_img/partners/vectorsoftware_150.jpg" width="150" height="35"></a><br>
+  <br>
+  As a provider of of software testing solutions for safety and business critical embedded applications, Vector Software exists to help you tackle the complexities of testing safety- and business-critical embedded software.  We are shaping the future of software test by: listening to our customers, addressing their challenges, and using their feedback to guide product enhancements.<br>
+  <br>
+  <br>
+  <a href="http://www.pure-systems.com/"><img src="_img/partners/pure.png" width="150" height="31" alt="Pure Systems"></a><br>
+  <br>
+  Pure-systems is the leading provider of software product line and variant management engineering tools and solutions. Pure-systems enables systems engineers, architects and developers to better manage the complexity within their software systems and speeds project delivery while maintaining quality and consistency. The core product,  provides organizations with a tool to manage variant-rich software systems and products.<br>
+  <br>
+  <br>
+  <a href="http://www.seppmed.de/"><img src="_img/partners/seppmed_small.jpg" width="150" height="34" alt="Sepp Med"></a><br>
+  <br>
+sepp.med ist ein mittel-ständisches, inhaber-geführtes Unternehmen, welches sich auf IT-Lösungen mit integrierter Qualitätssicherung in komplexen, sicherheits-kritischen Bereichen spezialisiert hat. Wir setzen auf strukturiertes Vorgehen, konsequent nach den Maßgaben der Kunden und den Anforderungen des jeweiligen Projekts mit zielgerichtetem Einsatz von Prozessen, Methoden und Werkzeugen.<br>
+</p>
+<p><a href="http://www.exept.de/"><img src="_img/partners/exept.png" width="110" height="86" alt="Exept"></a><br>
+  <br>
+  eXept offers proven unique software solutions for quality assurance management and test automation. Our programs can be customized to meet the specific needs of your company and the applicable requirements. The company was founded in 1996 with the mission to develop software solutions for complex tasks applying the highest standards.<br>
+  <br>
+  <br>
+  <a href="http://www.cip-delta.eu/"><img src="_img/partners/cip.png" width="89" height="100" alt="CIP"></a><br>
+  <br>
+Center for Innovative Processes.  Die Dynamik der Veränderungen um uns herum ist atemberaubend –  technologisch, politisch, wirtschaftlich, gesellschaftlich. Qualität bekommt in diesen unruhigen Zeiten eine neue Dimension: Qualität des Unternehmens. Das Streben nach Spitzenprodukten reicht nicht mehr.<br>
+<br>
+</p>
+<p><a href="http://www.it-designers.de/"><img src="_img/partners/homelogo_itd1.png" width="90" height="90" alt="ITD"></a></p>
+<p>Als Teil einer starken Firmengruppe mit engen Kontakten zu Industrie und Bildungseinrichtungen verfügen wir über langjährige Erfahrungen in der Automatisierungstechnik und der Automobilindustrie. Hier unterstützen wir unsere Kunden mit modernen Technologien und Prozessen bei der Konzeption und Entwicklung von Software sowie beim Betrieb von Applikationen.</p>
+<p><br>
+</p>
 </body>
 </html>
