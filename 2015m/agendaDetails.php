89aa4c5906dc8bbd3bc9d64b5bc024c1f0db00d6
Scott Richards <scott.srichards@gmail.com>
2015-10-04 09:38:57 -0700

    Add Agenda Details Fix Up Main Page check in Images.

---
+++ 2015m/agendaDetails.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	
+	require '../vendor/autoload.php';
+ 
+	use Parse\ParseClient;
+    use Parse\ParseObject;
+	use Parse\ParseQuery;
+ 
+	ParseClient::initialize('ytC7lrxAdRDvguofSHtRLyPe8DdiO5MdC4VdMDoz', 'iEkDJrIdoFFaGZrO2aCFWHJC0uBMbmV3DJxS0rSs', '0VA3z2dv24ilo3Iib400H2Zm7nsGLi5gZByH20ON');
+	
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Polarion Live 2014 Dashboard</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../jquery.mobile-1.4.4/jquery.mobile-1.4.4.min.css" rel="stylesheet" type="text/css" />
+	<link href="../_css/adminStyles.css" rel="stylesheet" type="text/css" />
+	<link href="../_css/sessionDetails.css" rel="stylesheet" type="text/css" />
+  <script src="../jquery.mobile-1.4.4/jquery.min.js"></script>
+	<script src="../jquery.mobile-1.4.4/jquery.mobile-1.4.4.min.js"></script>
+</head>
+
+<body>
+<div data-role="page">
+
+	<div data-role="header">
+		<h2><a href="../index.html"><img class="polarion-logo" src="../_img/polarion_logo_240x264.gif" width="62" height="67" alt="Polarion" /></a><span class="heading">Session Details</span> </h2>
+	</div><!-- /header -->
+	<div role="main" class="ui-content">
+	<?php 
+function getTime($start,$end) {
+	$timeStr = "";
+	if (!is_null($start)) {
+		$timeStr = $start->format("g:i");
+	}
+	if (!is_null($end)) {
+		$timeStr .=  " - " .  $end->format("g:i");
+	}
+  return $timeStr;
+}
+
+	$query = new ParseQuery("Agenda");
+	$objectId = $_GET["id"];
+//	echo "ObjectId = " . $_GET["id"];
+	if (!is_null($objectId)) {
+		$sessionObject = $query->get($objectId);
+		echo "<div class=\"sessionName\">" . $sessionObject->get("session") . "</div>\n";
+		echo "<div class=\"sessionTime\">Time: " . getTime($sessionObject->get("start"),$sessionObject->get("end")) . "</div>\n";
+		if (!is_null($sessionObject->get("location"))) {
+			echo "<div class=\"sessionLocation\">Location: " . $sessionObject->get("location") . "</div>\n";
+		}
+		if (!is_null($sessionObject->get("description"))) {
+			echo "<div class=\"sessionDescription\">Description: " . $sessionObject->get("description") . "</div>\n";
+		}
+		if (!is_null($sessionObject->get("presenter"))) {
+			echo "<div class=\"sessionSpeakers\">Presenter(s): " . $sessionObject->get("presenter") . "</div>\n";
+		}
+	} 
+  ?> 
+		
+  <div>
+<form action="../m/sessionRating.php" <?php if ($sessionObject->get("icon") == "trophy" || $sessionObject->get("icon") == "coffee" || $sessionObject->get("icon") == "food") echo " class=\"hidden\"" ?> method="post">
+		<p>
+		  <label for="speaker">Speaker:</label>
+      <input name="presenterRating" type="text" id="presenterRating" max="5" min="0" value="0" size="5">(1=Not Good, 5 = Excellent)<br />
+		  <label for="content">Content:</label>
+		  <input name="contentRating" type="text" id="contentRating" max="5" min="0" value="0" size="5"> (1=Not Good, 5 = Excellent)
+		</p>
+		<p class="centered">
+		  <input type="submit" name="button" id="button" value="Rate" />
+  </p>
+  <input name="session" type="hidden" value="<?php echo $objectId; ?>" />
+</form>
+  </div>
+	</div><!-- /content -->
+
+</div><!-- /page -->
+
+</body>
+</html>
