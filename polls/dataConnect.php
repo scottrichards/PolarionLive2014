1513ac378edae1a36659bfd8946243859d4dac89
Scott Richards <scott.srichards@gmail.com>
2015-10-22 09:11:52 -0700

    output answers

--- polls/dataConnect.php
+++ polls/dataConnect.php
@@ -43,7 +43,6 @@ $query_Recordset1 = "SELECT * FROM poll_question";
 $query_limit_Recordset1 = sprintf("%s LIMIT %d, %d", $query_Recordset1, $startRow_Recordset1, $maxRows_Recordset1);
 $Recordset1 = mysql_query($query_limit_Recordset1, $bcContent) or die(mysql_error());
 $row_Recordset1 = mysql_fetch_assoc($Recordset1);
-
 if (isset($_GET['totalRows_Recordset1'])) {
   $totalRows_Recordset1 = $_GET['totalRows_Recordset1'];
 } else {
@@ -51,6 +50,9 @@ if (isset($_GET['totalRows_Recordset1'])) {
   $totalRows_Recordset1 = mysql_num_rows($all_Recordset1);
 }
 $totalPages_Recordset1 = ceil($totalRows_Recordset1/$maxRows_Recordset1)-1;
+
+
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -70,6 +72,28 @@ $totalPages_Recordset1 = ceil($totalRows_Recordset1/$maxRows_Recordset1)-1;
 		print "<hr/>"; 
 		
   } while ($row_Recordset1 = mysql_fetch_assoc($Recordset1));
+
+	// Query answers
+	// ------------------
+	
+		$query_Answers = "SELECT answer, `ordinal`
+		FROM `poll_answer`
+		WHERE poll_answer.question_id = 1490656";
+		$query_limit_AnswerRecordset = sprintf("%s LIMIT %d, %d", $query_Answers, 0, 20);
+		$answersRecordsetQuery = mysql_query($query_limit_AnswerRecordset, $bcContent) or die(mysql_error());
+		$row_AnswersRecordset = mysql_fetch_assoc($answersRecordsetQuery);
+		$all_AnswersRecordset = mysql_query($query_Answers);
+		$totalRows_AnswersRecordset = mysql_num_rows($all_AnswersRecordset);
+		$totalPages_AnswersRecordset = ceil($totalRows_AnswersRecordset/20)-1;
+		do { 
+			print "<p>ordinal: " .  $row_AnswersRecordset['ordinal'] . "</p>";
+			print "<p>question: ";
+			print $row_AnswersRecordset['answer'];
+			print "</p>";
+			print "<hr/>"; 
+			
+		} while ($row_AnswersRecordset = mysql_fetch_assoc($answersRecordsetQuery));
+	
  ?>
 <form>
   <p>
